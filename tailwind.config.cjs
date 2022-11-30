a78517ee9cdf5605bcd44280ac7211e1bda6ea42
anyushu <anyushu0526@gmail.com>
2022-11-30 13:09:19 +0900

    install tailwindcss

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./public/**/*.{js,ts,html}",
+    "./src/**/*.{js,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
